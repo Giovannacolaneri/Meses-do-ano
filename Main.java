b6f7fbcaef99add596b0806bdaff846f5b2c7271
GiovannaColaneri <giovannacolanericbg@gmail.com>
2025-11-11 19:21:34 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+
+// Declaração do Enum "Mes"
+// Cada constante representa um mês do ano.
 enum Mes {
     JANEIRO,
     FEVEREIRO,
@@ -13,12 +16,17 @@ enum Mes {
     DEZEMBRO
 }
 
+// Classe principal
 public class Main {
     public static void main(String[] args) {
+        
+        // Criação de uma variável do tipo Mes e atribuição de um valor específico
         Mes mesAtual = Mes.NOVEMBRO;
 
+        // Exibe o mês atual
         System.out.println("Mês atual: " + mesAtual);
 
+        // Estrutura condicional para identificar a estação do ano
         switch (mesAtual) {
             case DEZEMBRO:
             case JANEIRO:
@@ -42,6 +50,7 @@ public class Main {
                 break;
         }
 
+        // Exibe todos os meses definidos no Enum
         System.out.println("\nLista completa de meses do ano:");
         for (Mes m : Mes.values()) {
             System.out.println("- " + m);
